62c5ef4c477bbdcbc4e2f0713985ca8f7bad491f
Jay Pratt <jp10010101010000@gmail.com>
2024-04-27 23:47:53 +1000

    Reduce brightness of indentation warning

--- lua/cypher1/init.lua
+++ lua/cypher1/init.lua
@@ -59,7 +59,7 @@ function Colors(color)
   color = color or "rose-pine-main"
   vim.cmd.colorscheme(color)
 
-  vim.api.nvim_set_hl(0, "Overnesting", { bg = "#500000" })
+  vim.api.nvim_set_hl(0, "Overnesting", { bg = "#400000" })
   vim.api.nvim_set_hl(0, "Normal", { bg = "black" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "black" })
 end
