01882ebe2b73ee0adcf90617e8cc8dfe5c8853b2
Jay Pratt <jp10010101010000@gmail.com>
2024-05-08 19:47:33 +1000

    Clean up netRW keybinding and make visual mode shove match normal mode

--- lua/cypher1/remap.lua
+++ lua/cypher1/remap.lua
@@ -1,9 +1,9 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- 'Shove': Move the visual selection
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>", {silent=true})
 vim.keymap.set("n", "˚", ":silent! m .-2<CR>", {silent=true})
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>", {silent=true})
