a3ad2b138f57f0d29d1347b37f696fa5663bd0d1
Jay Pratt <jp10010101010000@gmail.com>
2024-05-16 22:20:39 +1000

    Sync nvim tree with project.nvim

--- lua/cypher1/lazy.lua
+++ lua/cypher1/lazy.lua
@@ -406,8 +406,14 @@ local plugins = {
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
 
-      -- empty setup using defaults
-      require("nvim-tree").setup()
+      require("nvim-tree").setup({
+        sync_root_with_cwd = true,
+        respect_buf_cwd = true,
+        update_focused_file = {
+          enable = true,
+          update_root = true
+        },
+      })
     end,
   }, -- File manager
   "mhinz/vim-signify", -- Sign column diffs
