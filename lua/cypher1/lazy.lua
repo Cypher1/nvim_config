ccb1cc37302c85a2d43b5d53bcebc751bdf79c33
Jay Pratt <jp10010101010000@gmail.com>
2024-05-16 11:21:34 +1000

    Learn to love the snippet tools

--- lua/cypher1/lazy.lua
+++ lua/cypher1/lazy.lua
@@ -428,9 +428,9 @@ local plugins = {
   "hrsh7th/cmp-nvim-lsp",
   "hrsh7th/cmp-buffer",
   "hrsh7th/cmp-path",
-  -- "saadparwaiz1/cmp_luasnip",
-  -- "L3MON4D3/LuaSnip",
-  -- "rafamadriz/friendly-snippets",
+  "saadparwaiz1/cmp_luasnip",
+  "L3MON4D3/LuaSnip",
+  "rafamadriz/friendly-snippets",
 
   {
     "pmizio/typescript-tools.nvim",
--- lua/cypher1/lsp.lua
+++ lua/cypher1/lsp.lua
@@ -78,13 +78,13 @@ local cmp_select = {behavior = cmp.SelectBehavior.Select}
 
 -- this is the function that loads the extra snippets to luasnip
 -- from rafamadriz/friendly-snippets
--- require('luasnip.loaders.from_vscode').lazy_load()
+require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
     sources = {
         {name = 'path'},
         {name = 'nvim_lsp'},
-        -- {name = 'luasnip', keyword_length = 2},
+        {name = 'luasnip', keyword_length = 2},
         {name = 'buffer', keyword_length = 3},
     },
     mapping = cmp.mapping.preset.insert({
