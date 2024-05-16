ce24290cf7a8c962c2d0fc70dd17f0ffb6547aa9
Jay Pratt <jp10010101010000@gmail.com>
2024-05-16 11:20:17 +1000

    Add dependencies for Neogit

--- lua/cypher1/lazy.lua
+++ lua/cypher1/lazy.lua
@@ -68,6 +68,13 @@ local plugins = {
 
   {
     "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+      -- Only one of these is needed, not both.
+      "nvim-telescope/telescope.nvim", -- optional
+      -- "ibhagwan/fzf-lua",              -- optional
+    },
     keys = {
       {"<C-g>", "<cmd>Neogit<CR>", desc="Run an interactive git session"},
     },
