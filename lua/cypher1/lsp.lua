818304c9699c2a36d19aa12e635af05013278852
Jay Pratt <jp10010101010000@gmail.com>
2024-09-02 15:53:25 +1000

    Ensure lsp functions get names and have better bindings

--- lua/cypher1/lsp.lua
+++ lua/cypher1/lsp.lua
@@ -4,18 +4,13 @@ local augroup = vim.api.nvim_create_augroup
 autocmd('LspAttach', {
     group = augroup('user_lsp_attach', {clear = true}),
     callback = function(event)
-        local opts = {buffer = event.buf}
-
-        vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set('n', '<leader>h', function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set('n', '<leader>vs', function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set('n', '<C-d>', function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set('n', '<leader>vc', function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set('n', '<leader>vr', function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set('n', '<leader>vn', function() vim.lsp.buf.rename() end, opts)
-        vim.keymap.set('i', '<C-d>', function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, {buffer = event.buf, description = "LSP: Go to definition"})
+        vim.keymap.set('n', '<leader>R', function() vim.lsp.buf.code_action() end, {buffer = event.buf, description = "LSP code action"})
+        vim.keymap.set('n', '<leader>r', function() vim.lsp.buf.rename() end, {buffer = event.buf, description = "LSP rename symbol"})
+        vim.keymap.set('n', '<leader>h', function() vim.lsp.buf.hover() end, {buffer = event.buf, description = "LSP hover"})
+        vim.keymap.set('n', '<leader>l', function() vim.lsp.buf.reference() end, {buffer = event.buf, description = "LSP: Go to reference"})
+        vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, {buffer = event.buf, description = "LSP: Go to next diagnostic"})
+        vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, {buffer = event.buf, description = "LSP: Go to prev diagnostic"})
     end,
 })
 
